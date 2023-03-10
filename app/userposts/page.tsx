cbf4d91f7245374b38a8ce2299709544bc4eb7f2
spatulatom <spatulatom@gmail.com>
2023-03-10 10:19:37 +0000

    first commit

---
+++ app/userposts/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// const fetchAuthPosts = async () => {
+//     const response = await fetch(process.env.URL +"/userposts/api")
+//     const data = await response.json()
+//     return data
+//   }
+export default async function UserPosts() {
+
+    // const posts = await fetchAuthPosts()
+    // console.log('POSTS', posts)
+  return (
+    <div>UserPosts</div>
+  )
+}
