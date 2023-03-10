0e3de5e9aab4d5fcd228e0b01cb3f9f1d82ad74e
spatulatom <spatulatom@gmail.com>
2023-03-10 10:30:37 +0000

    fetch

--- app/userposts/page.tsx
+++ app/userposts/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-// const fetchAuthPosts = async () => {
-//     const response = await fetch(process.env.URL +"/userposts/api")
-//     const data = await response.json()
-//     return data
-//   }
+const fetchAuthPosts = async () => {
+    const response = await fetch(process.env.URL +"/userposts/api")
+    const data = await response.json()
+    return data
+  }
 export default async function UserPosts() {
 
-    // const posts = await fetchAuthPosts()
-    // console.log('POSTS', posts)
+    const posts = await fetchAuthPosts()
+    console.log('POSTS', posts)
   return (
     <div>UserPosts</div>
   )
