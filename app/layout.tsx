91e416e4e1b583f022443071b0197f482479ef67
spatulatom <spatulatom@gmail.com>
2023-03-12 14:53:00 +0000

    types exempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <body>
       <AuthContext>
         <QueryWrapper>
-           
+       {/* @ts-expect-error Async Server Component */}    
       <Nav />
       {children}
       </QueryWrapper>
