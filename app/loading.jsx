84792110de7909f613f5352177b7b4f121ccd94a
spatulatom <spatulatom@gmail.com>
2023-03-12 10:57:00 +0000

    loading

---
+++ app/loading.jsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+    // Or a custom loading skeleton component
+    return (
+      <div className="text-center h-60">  <i className="fa-solid fa-spinner fa-spin-pulse fa-2xl text-white"></i></div>
+    )}
