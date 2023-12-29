2286dddac089fca5fb151079e509885180f68c63
spatulatom <spatulatom@gmail.com>
2023-12-29 17:21:37 +0000

    comments

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,11 @@ import { notFound } from 'next/navigation';
 import Counter from './Counter';
 
 // NOT making Prisma calls here what is possible since this is a server
-// componet since we want to have more control over has this page is rendered
+// componet since we want to have more control over has this 
+// page is rendered
 //  - when Prisma call made here this page will be defult SSG.
-// FETCH from server components to our own backend requries full URL as oppose to partial
+// FETCH from server components to our own backend requries 
+// full URL as oppose to partial
 // form client components for example in AddPost.tsx
 
 const allPosts = async (): Promise<PostType[]> => {
@@ -33,18 +35,23 @@ let bla: Array<string>;
 bla = ['fasad'];
 
 const Home = async () => {
-  // const response: PostType[] = await allPosts(); //you can also set return data type here
+  // const response: PostType[] = await allPosts(); //you can also
+  // set return data type here
   const response = await allPosts();
 
   // when using state in client components we can do a check like this:
-  // response.length <= 0 && some JSX, can we do that here as well? Tes, we could but we wil use
+  // response.length <= 0 && some JSX, can we do that here as well?
+  // Tes, we could but we wil use
   // notFound instead
-  // side note: response.length && some JSX wont prevent rendering JSX when response = [] becuse
-  // event though response.length of empty array in JSX is 0, that is still A VALID VALUE in JSX
+  // side note: response.length && some JSX wont prevent rendering
+  // JSX when response = [] becuse
+  // event though response.length of empty array in JSX is 0, that
+  // is still A VALID VALUE in JSX
   if (!response) {
     notFound();
   }
-  // we can set title like this - not recommended - but only in clinet component
+  // we can set title like this - not recommended -
+  // but only in clinet component
   // document.title = "JavaScript DOM update"
   return (
     <div>
