ea755f0f9b8135ae7eedffc6f8d4643081ceb2df
spatulatom <spatulatom@gmail.com>
2024-02-04 21:15:09 +0000

    delpo 8

--- app/Counter.tsx
+++ app/Counter.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { PostType } from '../types/Post';
 import { memo } from 'react';
@@ -8,8 +7,7 @@ type count = {
 
 const Counter = ({ count }: any) => {
   return <h2 className="ml-2">All posts: {count.length}</h2>;
-}
-
+};
 
 // practicing mamo() and how it dosent prevent rerender in server componets decendnets
 // export default memo(Counter);
