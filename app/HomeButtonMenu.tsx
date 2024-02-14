86168771afdc53734fd48760ce17ee535ab60d92
spatulatom <spatulatom@gmail.com>
2024-02-14 12:52:53 +0000

    home butoon no need for session

--- app/HomeButtonMenu.tsx
+++ app/HomeButtonMenu.tsx
@@ -1,11 +1,11 @@
 "use client"
 import React from 'react'
 import { useRouter } from 'next/navigation'
-import { useSession } from 'next-auth/react';
+// import { useSession } from 'next-auth/react';
 
 export default function HomeButtonMenu() {
     const router = useRouter();
-    const { data } = useSession();
+    // const { data } = useSession();
   return (
     <li onClick={()=>router.replace('/')}>
         <h1 className="font-bold cursor-pointer md:text-lg italic text-teal-700 hover:text-white transition-all">Chat Room</h1>
