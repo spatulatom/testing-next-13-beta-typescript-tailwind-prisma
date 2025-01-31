e8a7c9047b7cb011eeb0af22f50ae3e780eaa8c4
spatulatom <spatulatom@gmail.com>
2025-01-31 11:38:37 +0000

    prisma config for: singleton pattern for Prisma Client

--- prisma/client.ts
+++ prisma/client.ts
@@ -4,20 +4,40 @@
 // if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
 // export default client
+// import { PrismaClient } from '@prisma/client'
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// const prisma = globalForPrisma.prisma ?? new PrismaClient({
+//   datasources: {
+//     db: {
+//       url: process.env.DATABASE_URL
+//     }
+//   }
+// })
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// export default prisma
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+// Declare prisma as a global variable to cache the PrismaClient
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL
-    }
-  }
-})
+let prisma: PrismaClient
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  // In development, we want to recreate the Prisma Client for each request
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
 
 export default prisma
