eb8cde0bac0571634a0b8ceb74994b216ebe9fd0
spatulatom <spatulatom@gmail.com>
2026-03-07 18:14:37 +0000

    chore: update dependencies and improve configuration settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   cacheComponents: true,
+    experimental: {
+    // Enable filesystem caching for `next dev`- till the default is switched 
+    // to `true` in a future release
+    turbopackFileSystemCacheForDev: true,
+    // Enable filesystem caching for `next build`
+    // turbopackFileSystemCacheForBuild: true,
+  },
   turbopack: {
     root: __dirname,
   },
