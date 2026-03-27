cc7883479bce2b75e0aca73660492750d182903f
spatulatom <spatulatom@gmail.com>
2026-03-27 10:37:39 +0000

    feat: add TypeScript and Prettier configurations to ESLint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
 import compat from 'eslint-plugin-compat';
 
 export default defineConfig([
   ...nextVitals,
+  ...nextTs,
+  prettier,
   {
     plugins: {
       compat,
